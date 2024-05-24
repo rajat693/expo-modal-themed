f893c35eefbbbc978d8c6878dacfa4aacc8c8d74
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-05-24 12:18:54 +0530

    feat: tested basic modal example

---
+++ App.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import {
+  GluestackUIProvider,
+  Text,
+  Box,
+  Button,
+  ButtonText,
+  Center,
+  CloseIcon,
+  Heading,
+  Icon,
+  Modal,
+  ModalBackdrop,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
+
+function ModalExample() {
+  const [showModal, setShowModal] = useState(false);
+  console.log(showModal);
+  const ref = React.useRef(null);
+  return (
+    <Center h={300}>
+      <Button onPress={() => setShowModal(true)} ref={ref}>
+        <ButtonText>Show Modal</ButtonText>
+      </Button>
+      <Modal
+        isOpen={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+        finalFocusRef={ref}
+      >
+        <ModalBackdrop />
+        <ModalContent>
+          <ModalHeader>
+            <Heading size="lg">Engage with Modals</Heading>
+            <ModalCloseButton>
+              <Icon as={CloseIcon} />
+            </ModalCloseButton>
+          </ModalHeader>
+          <ModalBody>
+            <Text>
+              Elevate user interactions with our versatile modals. Seamlessly
+              integrate notifications, forms, and media displays. Make an impact
+              effortlessly.
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              variant="outline"
+              size="sm"
+              action="secondary"
+              mr="$3"
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Cancel</ButtonText>
+            </Button>
+            <Button
+              size="sm"
+              action="positive"
+              borderWidth="$0"
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Explore</ButtonText>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Center>
+  );
+}
+
+export default function App() {
+  return (
+    <GluestackUIProvider config={config}>
+      <ModalExample />
+    </GluestackUIProvider>
+  );
+}
